07f29bd29fa86180c7c383457afbc58d37edc583
Alexander Petryakov <shurik_na@bk.ru>
2016-11-11 23:42:51 +0300

    Lesson1

---
+++ task1.2.rb
@@ -0,0 +1,15 @@
+# Program task 1.2 area of a triangle"
+
+puts "Введите значение основания треугольника в см."
+base = gets.to_f
+
+puts "Введите значение высоты треугольника в см."
+height = gets.to_f
+
+area = 0.5*base*height
+
+puts "Площадь треугольника составляет #{area} см2."
+
+
+
+
