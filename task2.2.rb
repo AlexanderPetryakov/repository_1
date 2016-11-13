df889d85072cb68db42494a8532e9d1f45d067a5
Alexander Petryakov <shurik_na@bk.ru>
2016-11-14 00:11:25 +0300

    Lesson 2

---
+++ task2.2.rb
@@ -0,0 +1,11 @@
+# program array 10-100 step 5
+
+i = 10
+new_array = []
+
+while i <= 100 do
+	new_array << i
+	i += 5
+end
+
+#puts new_array
---
+++ task2.3.rb
@@ -0,0 +1,13 @@
+# program Fibonacci numbers starting from 0 
+
+fib_array = [0]
+i = 1
+k = 0
+loop do
+	fib_array << i
+	i = fib_array[k] + fib_array[k + 1]
+	k += 1
+break if i > 100
+end
+
+#puts fib_array
