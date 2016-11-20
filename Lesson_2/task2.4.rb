96026f1f821e774f78678f641e0ec8d3b9418f9e
Alexander Petryakov <shurik_na@bk.ru>
2016-11-20 23:55:46 +0300

    Lesson 2 ver 1.1

--- Lesson_2/task2.4.rb
+++ Lesson_2/task2.4.rb
@@ -5,7 +5,7 @@ vowels2 = ["а", "е", "и", "о", "у", "ы", "э", "ю", "я"]
 alphabet = ('а'..'я')
 
 alphabet.each_with_index do |letter, index|
-  vowels1[letter] = index if vowels2.include?(letter)
+  vowels1[letter] = index + 1 if vowels2.include?(letter)
 end
 
 vowels1.each {|x, y| puts "#{x} - #{y}"}
